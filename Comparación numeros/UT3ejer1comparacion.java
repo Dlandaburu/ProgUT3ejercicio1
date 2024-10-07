9a817ad27292b0d1520db1a372d2305f7a0f7bca
Dlandaburu <mbutusi1@educacion.navarra.es>
2024-10-07 17:49:38 +0200

    Primera tarea que hago
    Unidad 3 ejercicio de comparación

---
+++ Comparación numeros/UT3ejer1comparacion.java
@@ -0,0 +1,12 @@
+ public class Main {
+class CompararNumeros;{
+public static void main (String[] args);{
+
+CompararNumeros(int numero1,
+int numero2);
+
+ if int numero1 > int numero2  System.out.print(El primer número es mayor);
+ else if int numero2 > int numero1 System.out.print(El segundo número es mayor);
+ else int numero1 = int numero2 system.out.print(Los dos números son iguales);}
+}
+}
