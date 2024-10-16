e24ccedc222a475d186c4135bbd60d174276021c
Dlandaburu <mbutusi1@educacion.navarra.es>
2024-10-16 10:45:27 +0200

    UT3Ejer1Comparacion
    Corrección del ejercicio anterior :)

---
+++ 1.1 ejercicio 1/Main.java
@@ -0,0 +1,9 @@
+public class CompararNumeros {
+    public static void main(String[] args) {
+
+        private int numero1; 
+        (numero1 = x + 1);
+        private int numero2;
+        (numero2 = x - 1);
+    }
+} 
